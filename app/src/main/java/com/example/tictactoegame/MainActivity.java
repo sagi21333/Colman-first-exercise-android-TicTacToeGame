e8356d24652d43d8c18375771fdba8ea2dd235df
asaf halely <asafhalely@gmail.com>
2022-12-04 20:16:04 +0200

    add line and play again

--- app/src/main/java/com/example/tictactoegame/MainActivity.java
+++ app/src/main/java/com/example/tictactoegame/MainActivity.java
@@ -2,34 +2,95 @@ package com.example.tictactoegame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    boolean gameActive = true;
-
     // Player representation
     // 0 - X
     // 1 - O
-    int activePlayer = 0;
-    int[] gameState = {2, 2, 2, 2, 2, 2, 2, 2, 2};
+    int activePlayer;
+    boolean gameActive;
+    int[] gameState = new int[9];
+    int counter;
 
-    int[][] winPositions = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8},
+    static int[][] winPositions = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8},
             {0, 3, 6}, {1, 4, 7}, {2, 5, 8},
             {0, 4, 8}, {2, 4, 6}};
-    public static int counter = 0;
+    
+    private void startGame() {
+        gameActive = true;
+        activePlayer = 0; // x goes first
+        counter = 0;
+        for (int i = 0; i < gameState.length; i++) {
+            gameState[i] = 2;
+        }
+    }
+    private void endGame(int position) {
+        gameActive = false;
+        Button playAgain= (Button) findViewById(R.id.playAgain);
+        playAgain.setVisibility(View.VISIBLE);
 
+        ImageView winLine = (ImageView) findViewById(R.id.winLine);
+        winLine.setRotation(0);
+
+        if (position == 0) {
+            winLine.setImageResource(R.drawable.mark3);
+            winLine.setRotation(90);
+        } else if(position == 1) {
+            winLine.setImageResource(R.drawable.mark4);
+            winLine.setRotation(90);
+        } else if(position == 2) {
+            winLine.setImageResource(R.drawable.mark5);
+            winLine.setRotation(90);
+        } else if(position == 3) {
+            winLine.setImageResource(R.drawable.mark3);
+        } else if(position == 4) {
+            winLine.setImageResource(R.drawable.mark4);
+        } else if(position == 5) {
+            winLine.setImageResource(R.drawable.mark5);
+        } else if(position == 6) {
+            winLine.setImageResource(R.drawable.mark1);
+        } else if(position == 7) {
+            winLine.setImageResource(R.drawable.mark2);
+        }
+    }
+
+    private int checkWin() {
+        for (int i = 0; i < 8; i++) {
+            if (gameState[winPositions[i][0]] == gameState[winPositions[i][1]] &&
+                    gameState[winPositions[i][1]] == gameState[winPositions[i][2]] &&
+                    gameState[winPositions[i][0]] != 2) {
+
+                //Todo: add mark the line of winning
+                endGame(i);
+
+                ImageView imgStatus = (ImageView) findViewById(R.id.imageStatus);
+
+                if (gameState[winPositions[i][0]] == 0) {
+                    imgStatus.setImageResource(R.drawable.xwin);
+                    return 0;
+                } else {
+                    imgStatus.setImageResource(R.drawable.owin);
+                    return 1;
+                }
+            }
+        }
+        return 2;
+    }
+    
     public void playerTap(View view) {
         ImageView img = (ImageView) view;
 
         int tappedImage = Integer.parseInt(img.getTag().toString());
 
-        //Todo: add a button of asking the player if he want to play a new game - "play again"
         if (!gameActive) {
-            gameReset(view);
+            return;
         }
 
         if (gameState[tappedImage] == 2) {
@@ -58,41 +119,17 @@ public class MainActivity extends AppCompatActivity {
             }
             img.animate().translationYBy(1000f).setDuration(300);
         }
-        int flag = 0;
-        // Check if any player has won
-        for (int[] winPosition : winPositions) {
-            if (gameState[winPosition[0]] == gameState[winPosition[1]] &&
-                    gameState[winPosition[1]] == gameState[winPosition[2]] &&
-                    gameState[winPosition[0]] != 2) {
-                flag = 1;
-
-                String winnerStr;
-
-                //Todo: add mark the line of winning
-                gameActive = false;
-                ImageView imgStatus= (ImageView) findViewById(R.id.imageStatus);
-
-                if (gameState[winPosition[0]] == 0) {
-                    imgStatus.setImageResource(R.drawable.xwin);
-                } else {
-                    imgStatus.setImageResource(R.drawable.owin);
-                }
-            }
-        }
-
-        if (counter == 9 && flag == 0) {
+        if (counter > 2 && counter < 9) {
+            checkWin();
+        } else if (counter == 9 && checkWin() == 2) {
             ImageView imgStatus= (ImageView) findViewById(R.id.imageStatus);
             imgStatus.setImageResource(R.drawable.nowin);
 
+            endGame(9);
         }
     }
 
     public void gameReset(View view) {
-        gameActive = true;
-        activePlayer = 0;
-        for (int i = 0; i < gameState.length; i++) {
-            gameState[i] = 2;
-        }
         // remove all the images from the boxes inside the grid
         ((ImageView) findViewById(R.id.imageView0)).setImageResource(0);
         ((ImageView) findViewById(R.id.imageView1)).setImageResource(0);
@@ -107,11 +144,19 @@ public class MainActivity extends AppCompatActivity {
         ImageView imgStatus= (ImageView) findViewById(R.id.imageStatus);
         imgStatus.setImageResource(R.drawable.xplay);
 
+        Button playAgain= (Button) findViewById(R.id.playAgain);
+        playAgain.setVisibility(View.INVISIBLE);
+
+        ImageView winLine= (ImageView) findViewById(R.id.winLine);
+        winLine.setImageResource(R.drawable.empty);
+
+        startGame();
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        startGame();
     }
 }
